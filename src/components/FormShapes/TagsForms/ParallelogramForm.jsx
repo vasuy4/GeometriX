9e7bd3a769df700f37c6b880dd0c141cbbe25660
Ryzhov Artem <ar.ryqp@mail.ru>
2024-04-23 07:14:48 +0300

    add parallelogram form

--- src/components/FormShapes/TagsForms/ParallelogramForm.jsx
+++ src/components/FormShapes/TagsForms/ParallelogramForm.jsx
@@ -12,6 +12,83 @@ export default function ParallelogramForm({handleFormSubmit, selectedShape, hand
         <button onClick={handleClose}>Close</button>
         <p>{selectedShape}</p>
         <img src={parallelogramImage} alt='parallelogram' />
+        <div className='form-group'>
+                <label htmlFor="side_a">Сторона a</label>
+                <input type="text" id="side_a" name="side_a" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="side_b">Сторона b</label>
+                <input type="text" id="side_b" name="side_b" />
+            </div>
+            
+            <div className='form-group'>
+                <label htmlFor="diameter1">Диагональ d1</label>
+                <input type="text" id="diameter1" name="diameter1" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="diameter2">Диагональ d2</label>
+                <input type="text" id="diameter2" name="diameter2" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="height">Высота h</label>
+                <input type="text" id="height" name="height" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="s">Площадь S</label>
+                <input type="text" id="s" name="s" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="perimeter">Периметр P</label>
+                <input type="text" id="perimeter" name="perimeter" />
+            </div>
+
+
+            <div className='form-group'>
+                <label htmlFor="alpha">Угол α</label>
+                <input type="text" id="alpha" name="alpha" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="betta">Угол β</label>
+                <input type="text" id="betta" name="betta" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="angle_y">Угол γ</label>
+                <input type="text" id="angle_y" name="angle_y" />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="angle_o">Угол δ</label>
+                <input type="text" id="angle_o" name="angle_o" />
+            </div>
+
+
+            <div className='form-group'>
+                <label htmlFor="points">Точка размещения</label>
+                <select name="points" id="points" defaultValue="O">
+                    <option value="O">O</option>
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                    <option value="C">C</option>
+                    <option value="D">D</option>
+                </select>
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="x">x</label>
+                <input type="text" id="x" name="x" defaultValue="0"/>
+            </div>
+            <div className='form-group'>
+                <label htmlFor="y">y</label>
+                <input type="text" id="y" name="y" defaultValue="0"/>
+            </div>
+            <button type="submit">Построить</button>
         </form>
     )
 }
