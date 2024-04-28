c391425ce9ffb38845e072d32ce6676202de09e7
Артём Рыжов <ar.ryqp@mail.ru>
2024-04-28 23:28:01 +0300

    add options

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,37 @@
-import {useState} from 'react'
-import logo from './LogoBlue.png'
-import './Header.css'
+import { useState } from 'react';
+import logo from './LogoBlue.png';
+import './Header.css';
 
-// Просто хедер, в котором находится лого, название и время.
 export default function Header() {
-    const [nowTime, setNowTime] = useState(new Date())
+    const [nowTime, setNowTime] = useState(new Date());
+    const [showOptions, setShowOptions] = useState(false);
 
-    setInterval(() => setNowTime(new Date()), 1000)
+    setInterval(() => setNowTime(new Date()), 1000);
 
-    return(
+    const toggleOptions = () => {
+        setShowOptions(!showOptions);
+    };
+
+    return (
         <header>
-            <a href="http://localhost:3000/" className='logolink'><img src={logo} alt="Logo"/></a>
-            <a href="http://localhost:3000/"><h3>GeometriX</h3></a>
+            <a href="http://localhost:3000/" className="logolink">
+                <img src={logo} alt="Logo" />
+            </a>
+            <a href="http://localhost:3000/">
+                <h3>GeometriX</h3>
+            </a>
+            <button id="optionsButton" onClick={toggleOptions}>Опции</button>
+            <div id="optionsPanel" className={showOptions ? '' : 'hidden'}>
+                <ul>
+                    <li>
+                        <button>Камера по умолчанию</button>
+                    </li>
+                    <li>
+                        <button>Вкл./Выкл. систему координат</button>
+                    </li>
+                </ul>
+            </div>
             <time>Time now: {nowTime.toLocaleTimeString()}</time>
         </header>
-    )
+    );
 }
