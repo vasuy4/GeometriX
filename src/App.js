7cb7a39ba53d86c7a2f4df118a2c84cda4317e1b
Артём Рыжов <ar.ryqp@mail.ru>
2024-06-09 18:45:49 +0300

    change in ui react-dom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from "react-router-dom";
 import Workbench from './Pages/WorkbenchPage/WorkbenchPage.jsx';
 import Home from './Pages/HomePage/HomePage.jsx'
-import { useState } from 'react';
-
 
 function App() {
-  const [currentPath, setCurrentPath]=useState(window.location.pathname)
-
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/workbench" element={<Workbench />} />
-        </Routes>
-
-        {currentPath === "/" && (
-        <div>
-          <Link to="/workbench">НАЧАТЬ!</Link>
-        </div>
-        )}
-
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/workbench" element={<Workbench />} />
+      </Routes>
     </Router>
   );
 }
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,14 @@
 import './HomePage.css';
+import { Link } from "react-router-dom";
+
 
 function Home() {
     return (
         <div className="Home">
             <h1>Добро пожаловать на наш сайт!</h1>
-            <p>Нажмите на кнопку НАЧАТЬ, чтобы начать работу.</p>
+            <p>Нажмите на кнопку <Link to="/workbench">НАЧАТЬ   </Link>, чтобы начать работу.</p>
+            
+            <Link to="/workbench">НАЧАТЬ!</Link>
         </div>
     );
 }
