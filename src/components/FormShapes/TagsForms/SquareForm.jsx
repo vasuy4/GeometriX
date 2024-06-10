224632ed5f48ae0f328b514dd9eb847b2569773c
Артём Рыжов <ar.ryqp@mail.ru>
2024-06-10 08:13:33 +0300

    change square form

--- src/components/FormShapes/TagsForms/SquareForm.jsx
+++ src/components/FormShapes/TagsForms/SquareForm.jsx
@@ -1,131 +1,89 @@
-import squareImage from '..//formShapesImg/square.svg';
-
-
-// Отображает форму квадрата
-export default function SquareForm({handleFormSubmit, selectedShape, handleClose}){
-    // Округление числа
-    const fixedNum = (num) => {
-        if (num.toFixed(4) === num){
-            return num
+import { fixedNum, toDegrees, toRadians, checkCalculate, checkBelowZero, cot } from '../formulas.js'
+import rhombImage from '../formShapesImg/square.svg'
+export default function RhombForm({handleFormSubmit, selectedShape, handleClose}) {
+    // Подсчёт параметров при известных стороне и высоте
+    const calcParamsSide = (a) => {
+        let d = Math.sqrt(2*a**2)
+        let S = a*a
+        let P = 4*a
+        let r = a/2.0
+        return [a, d, S, P, r]
+    }   
+    
+    // Проверка ввода корректных значений после нажатия кнопки построить
+    const handleFormSubmitCheckParameters = (event, selectedShape) => {
+        event.preventDefault()
+        let side_a = fixedNum(Number(document.getElementById('side_a').value))
+        let diagonal = fixedNum(Number(document.getElementById('diagonal').value))
+        let S = fixedNum(Number(document.getElementById('s').value))
+        let P = fixedNum(Number(document.getElementById('perimeter').value))
+        let r = fixedNum(Number(document.getElementById('r').value))
+        let arrInput = [side_a, diagonal, S, P, r]
+        const idInputs = ['side_a', 'diagonal', 's', 'perimeter', 'r']
+        // Проверка на то, что какое то число введено менише/равно нулю
+        const belowZero = checkBelowZero(arrInput, idInputs)
+        if (belowZero) return
+        
+        // Подсчёт остальных параметров, опираясь на:
+        // Сторону
+        if (side_a) {
+            let arrCheck = calcParamsSide(side_a)
+            checkCalculate(handleFormSubmit, event, selectedShape, arrInput, arrCheck, idInputs, 'side ok', 'side bad')
+        } 
+        // диагональ
+        else if (diagonal) {
+            side_a = Math.sqrt((diagonal**2)/2.0)
+            let arrCheck = calcParamsSide(side_a)
+            checkCalculate(handleFormSubmit, event, selectedShape, arrInput, arrCheck, idInputs, 'd ok', 'd bad')
         }
-        else {
-            return num.toFixed(4)
+        // площадь
+        else if (S) {
+            side_a = Math.sqrt(S)
+            let arrCheck = calcParamsSide(side_a)
+            checkCalculate(handleFormSubmit, event, selectedShape, arrInput, arrCheck, idInputs, 's ok', 's bad')
+        }
+        // периметр
+        else if (P) {
+            side_a = P/4.0
+            let arrCheck = calcParamsSide(side_a)
+            checkCalculate(handleFormSubmit, event, selectedShape, arrInput, arrCheck, idInputs, 'P ok', 'P bad')
+        }
+        // радиус вписанной окружности
+        else if (r) {
+            side_a = r*2
+            let arrCheck = calcParamsSide(side_a)
+            checkCalculate(handleFormSubmit, event, selectedShape, arrInput, arrCheck, idInputs, 'r ok', 'r bad')
         }
     }
     
-    // Обработчик изменения зависимых переменных
-    const handleInputChange = (event) => {
-        const inputName = event.target.name
-        let inputValue = event.target.value;
-        let inputElement;
-        let a;
-        if (inputValue && inputValue <= 0){
-            event.target.value = 1
-            inputValue = 1
-        }
-        switch (inputName) {
-            case 'side_a':
-                inputElement = document.getElementById('diameter');
-                inputElement.value = fixedNum(inputValue * Math.sqrt(2));
-
-                inputElement = document.getElementById('s');
-                inputElement.value = fixedNum(inputValue * inputValue);
-                
-                inputElement = document.getElementById('perimeter');
-                inputElement.value = fixedNum(inputValue * 4);
-
-                inputElement = document.getElementById('r');
-                inputElement.value = fixedNum(inputValue / 2);
-                break
-            case 'diameter':
-                a = inputValue / Math.sqrt(2);
-                inputElement = document.getElementById('side_a');
-                inputElement.value = fixedNum(a);
-
-                inputElement = document.getElementById('s');
-                inputElement.value = fixedNum(a * a);
-                
-                inputElement = document.getElementById('perimeter');
-                inputElement.value = fixedNum(a * 4);
-
-                inputElement = document.getElementById('r');
-                inputElement.value = fixedNum(a / 2);
-                break
-            case 's':
-                a = Math.sqrt(inputValue)
-                inputElement = document.getElementById('side_a');
-                inputElement.value = fixedNum(a);
-
-                inputElement = document.getElementById('diameter');
-                inputElement.value = fixedNum(a * Math.sqrt(2));
-                
-                inputElement = document.getElementById('perimeter');
-                inputElement.value = fixedNum(a * 4);
-
-                inputElement = document.getElementById('r');
-                inputElement.value = fixedNum(a / 2);
-                break
-            case 'perimeter':
-                a = inputValue / 4
-                inputElement = document.getElementById('side_a');
-                inputElement.value = fixedNum(a);
-
-                inputElement = document.getElementById('diameter');
-                inputElement.value = fixedNum(a * Math.sqrt(2));
-                
-                inputElement = document.getElementById('s');
-                inputElement.value = fixedNum(a * a);
-
-                inputElement = document.getElementById('r');
-                inputElement.value = fixedNum(a / 2);
-                break
-            case 'r':
-                a = inputValue * 2
-                inputElement = document.getElementById('side_a');
-                inputElement.value = fixedNum(a);
-
-                inputElement = document.getElementById('diameter');
-                inputElement.value = fixedNum(a * Math.sqrt(2));
-                
-                inputElement = document.getElementById('s');
-                inputElement.value = fixedNum(a * a);
-
-                inputElement = document.getElementById('perimeter');
-                inputElement.value = fixedNum(a * 4);
-                break
-            default:
-                console.log('undefinded input');
-    }}
-
     return (
-        <form onSubmit={(event) => handleFormSubmit(event, selectedShape)} action=''>
-            <button onClick={handleClose}>Close</button>
-            <p>{selectedShape}</p>
-            <img src={squareImage} alt="square" />
-
+        <form onSubmit={(event) => handleFormSubmitCheckParameters(event, selectedShape)} action=''>
+        <button onClick={handleClose}>Close</button>
+        <p>{selectedShape}</p>
+        <img src={rhombImage} alt='parallelogram' />
+        <div className='form-group'>
+                <label htmlFor="side_a">a</label>
+                <input type="text" id="side_a" name="side_a" />
+            </div>
+            
             <div className='form-group'>
-                <label htmlFor="side_a">Сторона a</label>
-                <input type="text" id="side_a" name="side_a" onChange={handleInputChange}/>
+                <label htmlFor="diagonal">d</label>
+                <input type="text" id="diagonal" name="diagonal" />
             </div>
 
             <div className='form-group'>
-                <label htmlFor="diameter">Диагональ</label>
-                <input type="text" id="diameter" name="diameter" onChange={handleInputChange}/>
+                <label htmlFor="s">S</label>
+                <input type="text" id="s" name="s" />
             </div>
 
             <div className='form-group'>
-                <label htmlFor="s">Площадь</label>
-                <input type="text" id="s" name="s" onChange={handleInputChange}/>
+                <label htmlFor="perimeter">P</label>
+                <input type="text" id="perimeter" name="perimeter" />
             </div>
 
             <div className='form-group'>
-                <label htmlFor="perimeter">Периметр</label>
-                <input type="text" id="perimeter" name="perimeter" onChange={handleInputChange}/>
-            </div>
-
-            <div className='form-group'>
-                <label htmlFor="r">Радиус вписанной окружности</label>
-                <input type="text" id="r" name="r" onChange={handleInputChange}/>
+                <label htmlFor="r">r</label>
+                <input type="text" id="r" name="r" />
             </div>
             <button type="submit">Построить</button>
         </form>
