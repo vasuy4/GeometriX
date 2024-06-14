2b545e2dcd7c7d174561a5d04b619a9c18c9a79e
Артём Рыжов <ar.ryqp@mail.ru>
2024-06-14 20:35:52 +0300

    new update for construction ree

--- src/Pages/WorkbenchPage/ConstructionTree.jsx
+++ src/Pages/WorkbenchPage/ConstructionTree.jsx
@@ -1,29 +1,17 @@
+import './ConstructionTree.css'
+
 export function ConstructionTree({ constructionTree, show }) {
 
     return (
         <div className="constructionTree">
             {constructionTree.map((shape, index) => (
                 <button key={index}>
-                    <img src={shape.shapeImage} alt={shape.shape} className="shape-image" />
-                    <span className="shape-name">{shape.shapeText}</span>
+                    <div key={index} className="shape-item">
+                        <img src={shape.shapeImage} alt={shape.shape} className="shape-image" />
+                        <span className="shape-name">{shape.shapeText}</span>
+                    </div>
                 </button>
             ))}
         </div>
     );
 }
-
-export function CossnstructionTree({ constructionTree, show }) {
-
-    return (
-        <div className="constructionTree">
-            {constructionTree.map((shape, index) => (
-                <div key={index} className="shape-item">
-                    <img src={shape.shapeImage} alt={shape.shape} className="shape-image" />
-                    <span className="shape-name">{shape.shapeText}</span>
-                </div>
-            ))}
-        </div>
-    );
-}
-
-
