aa822608642ba26c75e02a37153ef61bc09eb522
Артём Рыжов <ar.ryqp@mail.ru>
2024-09-06 17:18:24 +0300

    add colors ege2 stage1-2

--- src/Pages/Levels/EGELEvelsScenarios.js
+++ src/Pages/Levels/EGELEvelsScenarios.js
@@ -162,9 +162,18 @@ export function egeLevel2(nowStage, angleASB=36, baseSide=7) {
     //     `AB = AC = BM => треугольник AMB равносторонний со стороной ${fixedNum(baseSide)}. Найдём его площадь<br>S = (AB^2 * 3^(1/2)) / 4 = (${fixedNum(baseSide)}^2 * 3^(1/2)) / 4 = ${answer}<br><b><u>ОТВЕТ: ${answer}</b></u>`
     // ]
 
+    const colorAngleASB = hexColorToBabylonColors("#70ff4c")  // green
+    const colorSC = hexColorToBabylonColors("#FFBC40")  // orange
+    const colorAM = hexColorToBabylonColors("#4cccff")  // lightblue 
+    const colorBaseSide = hexColorToBabylonColors("#f478ff")  // pink
+    const colorAngleASC = hexColorToBabylonColors("#a1fce1")  // lightgreen
+    const colorAngleSAC = hexColorToBabylonColors("#00b7ff")  // blue
+    const colorAngleSCA = hexColorToBabylonColors("#2de7ff")  // lightblue
+    const colorAngleMAC = hexColorToBabylonColors("#c0ff2d")  // lime
+
     const text = [
-        `В правильной треугольной пирамиде SABC с основанием ABC угол ASB равен ${fixedNum(angleASB)}°. На ребре SC взята точка M так, что AM биссектриса угла SAC. Сторона основания ${fixedNum(baseSide)}. Найдите площадь сечения пирамиды, проходящего через точки A, M и B.`,
-        `Для нахождения площади сечения, найдём все стороны треугольника AMB.<br>Рассмотрим треугольник ASC. Пирамида SABC является правильной, поэтому: ∠ASC = ∠ASB = ${fixedNum(angleASB)}°, и<br>∠SAC = ∠SCA = (180° - ∠ASC)/2 = ${fixedNum(angleSAC)}°<br>∠MAC = ∠SAC/2 = ${fixedNum(angleMAC)}°`,
+        `В правильной треугольной пирамиде SABC с основанием ABC угол ASB равен <span style="color: #70ff4c">${fixedNum(angleASB)}°</span>. На ребре <span style="color: #FFBC40">SC</span> взята точка M так, что <span style="color: #4cccff">AM</span> биссектриса угла SAC. Сторона основания <span style="color: #f478ff">${fixedNum(baseSide)}</span>. Найдите <u>площадь сечения пирамиды</u>, проходящего через точки A, M и B.`,
+        `Для нахождения площади сечения, найдём все стороны треугольника AMB.<br>Рассмотрим треугольник ASC. Пирамида SABC является правильной, поэтому: <span style="color: #a1fce1">∠ASC</span> = <span style="color: #70ff4c">∠ASB</span> = <span style="color: #a1fce1">${fixedNum(angleASB)}°</span>, и<br><span style="color: #00b7ff">∠SAC</span> = <span style="color: #2de7ff">∠SCA</span> = (180° - <span style="color: #a1fce1">∠ASC</span>)/2 = <span style="color: #00b7ff">${fixedNum(angleSAC)}°</span><br><span style="color: #c0ff2d">∠MAC</span> = <span style="color: #00b7ff">∠SAC</span>/2 = <span style="color: #c0ff2d">${fixedNum(angleMAC)}°</span>`,
         `Теперь рассмотрим треугольник CAM<br>∠AMC = 180° - ∠MAC - ∠SCA = 180° - ${fixedNum(angleMAC)}° - ${fixedNum(angleSAC)}° = ${fixedNum(angleAMC)}°`,
         `Найдём AM по теореме синусов<br>AC/sin(∠AMC) = AM/sin(∠MCA). Выразим:<br>AM = AC*sin(∠MCA) / sin(∠AMC) = ${fixedNum(baseSide)} * sin(${fixedNum(angleSAC)}°) / sin(${fixedNum(angleAMC)}°) = ${fixedNum(AM)}`,
         `Докажем, что треугольники AMC и BMC равны<br>1) Пирамида SABC правильная => все углы в основании треугольников равны => ∠SCA = ∠SCB<br>2) BC = AC, т.к. в основании правильной пирамиды лежит равносторонний треугольник<br>3) MC - общая сторона<br>Следовательно треугольники AMC и BMC равны по двум сторонам и углу между ними.`,
@@ -209,9 +218,12 @@ export function egeLevel2(nowStage, angleASB=36, baseSide=7) {
     
     const BSparams = [...B, ...S, [1,1,1]]
     const ASparams = [...A, ...S, [1,1,1]]
+    const CSparams2 = [...C, ...S, colorSC]
     const CSparams = [...C, ...S, [1,1,1]]
 
+
     const BMparams = [...B, ...M, [1,1,1]]
+    const AMparams2 = [...A, ...M, colorAM]
     const AMparams = [...A, ...M, [1,1,1]]
 
     const eqAMparams = [middleAM[0], middleAM[1]-sizeText/5, middleAM[2], middleAM[0], middleAM[1]+sizeText/5, middleAM[2], [1,1,1]]
@@ -224,6 +236,7 @@ export function egeLevel2(nowStage, angleASB=36, baseSide=7) {
     const Mparams = ['M', "#FFFFFF", sizeText, M[0]+sizeText/5, M[1], M[2], 0, toRadians(180), 0]
     const Sparams = ['S', "#FFFFFF", sizeText, ...S, 0, toRadians(180), 0]
 
+    const valABparams2 = [`${fixedNum(baseSide)}`, "#f478ff", sizeText, middleAB[0]-sizeText/2.7, middleAB[1], middleAB[2]-sizeText/8, toRadians(90), toRadians(120), 0]
     const valABparams = [`${fixedNum(baseSide)}`, "#FFFFFF", sizeText, middleAB[0]-sizeText/2.7, middleAB[1], middleAB[2]-sizeText/8, toRadians(90), toRadians(120), 0]
     const valACparams = [`${fixedNum(baseSide)}`, "#FFFFFF", sizeText, middleAC[0]+sizeText/2.5, 0, middleAC[2]-sizeText/3, toRadians(90), 0, 0]
     const valAMparams = [`${fixedNum(AM)}`, "#FFFFFF", sizeText, middleAM[0]+sizeText/3, middleAM[1]+sizeText/2, middleAM[2], toRadians(90-angleSAC), 0, toRadians(angleMAC)]
@@ -235,19 +248,28 @@ export function egeLevel2(nowStage, angleASB=36, baseSide=7) {
     const shiftZforSCB = sizeText/1.4
     const shiftXforSCB = 0.5 * shiftZforSCB / Math.sin(toRadians(60))
     
-    const valAngleCSBparams = [`${fixedNum(angleASB)}°`, "#FFFFFF", sizeText*0.8, S[0]-shiftXforCSB, S[1]-shiftZforCSB, S[2], toRadians(90-angleSAC), toRadians(120), 0]
-    const valAngleASCparams = [`${fixedNum(angleASB)}°`, "#FFFFFF", sizeText*0.8, S[0]+sizeText/8, S[1]-shiftZforCSB, S[2]-shiftXforCSB, toRadians(90-angleSAC), 0, 0]
-    const valAngleSACparams = [`${fixedNum(angleSAC)}°`, "#FFFFFF", sizeText, -sizeText/3, A[1]+sizeText/2, A[2], toRadians(90-angleSAC), 0, 0]
-    const valAngleMACparams = [`${fixedNum(angleMAC)}°`, "#FFFFFF", sizeText*0.8, A[0]+sizeText*1.2, A[1]+sizeText/5, A[2], toRadians(90-angleSAC), 0, 0]
+    const valAngleCSBparams = [`${fixedNum(angleASB)}°`, "#70ff4c", sizeText*0.8, S[0]-shiftXforCSB, S[1]-shiftZforCSB, S[2], toRadians(90-angleSAC), toRadians(120), 0]
+    const valAngleASCparams = [`${fixedNum(angleASB)}°`, "#a1fce1", sizeText*0.8, S[0]+sizeText/8, S[1]-shiftZforCSB, S[2]-shiftXforCSB, toRadians(90-angleSAC), 0, 0]
+    const valAngleSACparams = [`${fixedNum(angleSAC)}°`, "#FFFFFF", sizeText, -sizeText/3, A[1]+sizeText/2, A[2], toRadians(90-angleSAC), 0, 0] 
+    const valAngleSACparams2 = [`${fixedNum(angleSAC)}°`, "#00b7ff", sizeText, -sizeText/3, A[1]+sizeText/2, A[2], toRadians(90-angleSAC), 0, 0] //00b7ff
+
+    const valAngleMACparams = [`${fixedNum(angleMAC)}°`, "#FFFFFF", sizeText*0.8, A[0]+sizeText*1.2, A[1]+sizeText/5, A[2], toRadians(90-angleSAC), 0, 0] 
+    const valAngleMACparams2 = [`${fixedNum(angleMAC)}°`, "#c0ff2d", sizeText*0.8, A[0]+sizeText*1.2, A[1]+sizeText/5, A[2], toRadians(90-angleSAC), 0, 0]  //c0ff2d
+
     const valAngleSCBparams = [`${fixedNum(angleSAC)}°`, "#FFFFFF", sizeText, C[0]-shiftXforSCB, C[1]+sizeText/5, C[2]+shiftZforSCB, toRadians(90-angleSAC), toRadians(-120), 0]
 
     const normalVectorMAC = calculateNormalVector(M, A, C)
 
     const angleMACparams = [...A, sizeText*0.8, 0, toRadians(angleMAC), 1, 0, ...normalVectorMAC]
+    const angleMACparams2 = [...A, sizeText*0.8, 0, toRadians(angleMAC), 1, 0, ...normalVectorMAC, 1, colorAngleMAC]
     const angleMASparams = [...A, sizeText*0.7, toRadians(angleMAC*2), toRadians(angleMAC)*1.2, 1, 0, ...normalVectorMAC, -1]
 
     const angleSACparams = [...A, sizeText*1.5, 0, toRadians(angleMAC*2)*1.05, 2, 0.2, ...normalVectorMAC]
+    const angleSACparams2 = [...A, sizeText*1.5, 0, toRadians(angleMAC*2)*1.05, 2, 0.2, ...normalVectorMAC, 1, colorAngleSAC]
+
     const angleSCAparams = [...C, sizeText*0.8, toRadians(180), toRadians(angleMAC*2)*1.05, 2, 0.2, ...normalVectorMAC, -1]
+    const angleSCAparams2 = [...C, sizeText*0.8, toRadians(180), toRadians(angleMAC*2)*1.05, 2, 0.2, ...normalVectorMAC, -1, colorAngleSCA]
+
 
     let countArcs = 3
     if (angleSAC === angleAMC) countArcs = 2
@@ -263,15 +285,15 @@ export function egeLevel2(nowStage, angleASB=36, baseSide=7) {
         'line3d_3': ACparams,
         'line3d_4': BSparams,
         'line3d_5': ASparams,
-        'line3d_6': CSparams,
+        'line3d_6': CSparams2,
         'line3d_7': BMparams,
-        'line3d_8': AMparams,
+        'line3d_8': AMparams2,
         'createTextPlane': Aparams,
         'createTextPlane_1': Bparams,
         'createTextPlane_2': Cparams,
         'createTextPlane_3': Mparams,
         'createTextPlane_4': Sparams,
-        'createTextPlane_5': valABparams,
+        'createTextPlane_5': valABparams2,
         'createTextPlane_6': valAngleCSBparams,
         'createAngle3d_1': angleMACparams,
         'createAngle3d_2': angleMASparams,
@@ -295,12 +317,12 @@ export function egeLevel2(nowStage, angleASB=36, baseSide=7) {
         'createTextPlane_5': valABparams,
         'createTextPlane_6': valAngleCSBparams,
         'createTextPlane_7': valAngleASCparams,
-        'createTextPlane_8':valAngleSACparams,
-        'createTextPlane_9':valAngleMACparams,
-        'createAngle3d_1': angleMACparams,
+        'createTextPlane_8':valAngleSACparams2,
+        'createTextPlane_9':valAngleMACparams2,
+        'createAngle3d_1': angleMACparams2,
         'createAngle3d_2': angleMASparams,
-        'createAngle3d_3': angleSACparams,
-        'createAngle3d_4': angleSCAparams,
+        'createAngle3d_3': angleSACparams2,
+        'createAngle3d_4': angleSCAparams2,
     }, {
         'fieldClear': [],
         'light': [0, -1, 0],
