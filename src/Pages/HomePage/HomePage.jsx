2fafe1206ce21e97a51ae81472399e9c25b4f9fe
Артём Рыжов <ar.ryqp@mail.ru>
2024-09-08 19:00:45 +0300

    baseSite

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
 import './HomePage.css';
 import { Link } from "react-router-dom";
 import logo from "../../components/Header/LogoBlue.png";
+import image1 from './images/image128.png'
+import image2 from "./images/image129.png"; 
 
 function Home() {
     return (
         <div className="Home">
             <header className='headerHome'>
                 <div className='logoHome'>
-                    <Link href="/" className="logolink">
+                    <Link to="/" className="logolink">
                         <img src={logo} alt="Logo" />
                     </Link>
                     <Link to="/">
@@ -23,22 +25,29 @@ function Home() {
                     </nav>
                 </div>
             </header>
-            
 
-            <div className='programms'>
-                <h3>Геометрический 3D-калькулятор</h3>
-                <p>Графическая форма представления геометрических фигур с использованием трёхмерной графики</p>
+            <div className="grid-container">
+                <div className="grid-item item1">
+                    <h3 className='headGrid'>Геометрический 3D-калькулятор</h3>
+                    <p className='descrGrid'>Графическая форма представления геометрических фигур с использованием трёхмерной графики</p>
+                    <Link to="/workbench/calculcator" className="btnGrid">Перейти к калькулятору</Link>
+                </div>
+                <div className="grid-item item2">
+                    <img src={image1} className='imgGrid' alt="3D Calculator Screenshot" />
+                </div>
+                <div className="grid-item item3">
+                    <img src={image2} className='imgGrid' alt="Another Program Screenshot" />
+                </div>
+                <div className="grid-item item4">
+                    <h3 className='headGrid'>Геометрический 3D-учебник</h3>
+                    <p className='descrGrid'>Графическая, интерактивная форма представления геометрических задач с использованием трёхмерной графики.</p>
+                    <Link to="/ChoosingDifficulty" className="btnGrid">Открыть учебник</Link>
+                </div>
             </div>
 
-
-            <h1>Добро пожаловать на наш сайт!</h1>
-            <p>Нажмите на кнопку <Link to="/workbench/calculator">НАЧАТЬ   </Link>, чтобы начать работу.</p>
             
-            <Link to="/workbench/calculcator">НАЧАТЬ!</Link>
-            <Link to="/ChoosingDifficulty">Интерактивный учебник</Link>
         </div>
     );
 }
-
 
 export default Home;
